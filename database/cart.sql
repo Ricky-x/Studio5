2131350acc7843922355ae54da8b6cf33eaa86a6
Ricky-x <109962938+Ricky-x@users.noreply.github.com>
2023-04-07 09:34:13 +1200

    Add files via upload

---
+++ database/cart.sql
@@ -0,0 +1,7 @@
+INSERT INTO `cart` (`id`, `order_no`, `product_id`, `user_id`, `create_at`, `count`) VALUES (40, '642ac20e4355c', 6, 18, '2023-04-03 20:09:50', 2);
+INSERT INTO `cart` (`id`, `order_no`, `product_id`, `user_id`, `create_at`, `count`) VALUES (42, '642ac22366445', 1, 18, '2023-04-03 20:10:11', 1);
+INSERT INTO `cart` (`id`, `order_no`, `product_id`, `user_id`, `create_at`, `count`) VALUES (43, '642ac22926e69', 5, 18, '2023-04-03 20:10:17', 1);
+INSERT INTO `cart` (`id`, `order_no`, `product_id`, `user_id`, `create_at`, `count`) VALUES (44, '642ac31db3894', 3, 18, '2023-04-03 20:14:21', 1);
+INSERT INTO `cart` (`id`, `order_no`, `product_id`, `user_id`, `create_at`, `count`) VALUES (47, '642b00638a949', 8, 20, '2023-04-04 00:35:47', 2);
+INSERT INTO `cart` (`id`, `order_no`, `product_id`, `user_id`, `create_at`, `count`) VALUES (48, '642b07f846ec7', 3, 21, '2023-04-04 01:08:08', 1);
+INSERT INTO `cart` (`id`, `order_no`, `product_id`, `user_id`, `create_at`, `count`) VALUES (49, '642b07fc041c0', 8, 21, '2023-04-04 01:08:12', 1);
---
+++ database/supermarkets.sql
@@ -0,0 +1,3 @@
+INSERT INTO `supermarkets` (`id`, `name`) VALUES (1, 'Countdown');
+INSERT INTO `supermarkets` (`id`, `name`) VALUES (2, 'Pak\'nSave');
+INSERT INTO `supermarkets` (`id`, `name`) VALUES (3, 'New World');
